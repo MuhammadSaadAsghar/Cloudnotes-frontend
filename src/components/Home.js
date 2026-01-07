8ce7d87cff8964ac8375cfd84bc418c7cce913b0
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-01-07 11:41:20 +0500

    Adding awesome font icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <h2>Add Notes</h2>
         <form>
           <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label htmlFor="exampleInputEmail1">Email address</label>
             <input
               type="email"
               className="form-control"
@@ -23,7 +23,7 @@ const Home = () => {
             </small>
           </div>
           <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
+            <label htmlFor="exampleInputPassword1">Password</label>
             <input
               type="password"
               className="form-control"
@@ -37,7 +37,7 @@ const Home = () => {
               className="form-check-input"
               id="exampleCheck1"
             />
-            <label className="form-check-label" for="exampleCheck1">
+            <label className="form-check-label" htmlFor="exampleCheck1">
               Check me out
             </label>
           </div>
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -3,12 +3,16 @@ import React from "react";
 function Noteitem(props) {
   const { note } = props;
   return (
-    <div className="col-md-3"> 
+    <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
+          <div className="d-flex align-items-center">
+            <h5 className="card-title">
+              {note.title} <i className="far fa-trash-alt mx-2 "></i>
+              <i className="far fa-edit mx-2"></i>
+            </h5>
+          </div>
           <p className="card-text"> {note.description}</p>
-         
         </div>
       </div>
     </div>
